ddd0efc00a8df922021a93d3a0249226f90304d3
Jammerjab1 <89883568+Jammerjab1@users.noreply.github.com>
2021-10-20 07:23:33 -0400

    Added Splash sound effect upon reset + Built again

--- Assets/scripts/KillFloor.cs
+++ Assets/scripts/KillFloor.cs
@@ -7,8 +7,16 @@ public class KillFloor : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawn_point;
 
+    public AudioSource tickSource;
+
+    void Start () {
+
+        tickSource = GetComponent<AudioSource> ();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         player.transform.position = respawn_point.transform.position;
+        tickSource.Play ();
     }
 }
